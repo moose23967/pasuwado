c40fd16095ba79869613cb285f11d630445ae955
moose23967 <moose23967@gmail.com>
2024-09-26 20:16:43 +0300

    Switched to Handlebars

--- pasuwado/__main__.py
+++ pasuwado/__main__.py
@@ -1,13 +1,19 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from typing import Any
+
+from pybars import Compiler
 
 from .generator import Generator
 from .randomizer import Randomizer
 
-index_content: str
+index_template: Any
 styles_content: bytes
 
-with open("pasuwado/index.html") as index_file:
+with open("pasuwado/index.hbs") as index_file:
     index_content = index_file.read()
+
+    index_template_compiler = Compiler()
+    index_template = index_template_compiler.compile(index_content)
 
 with open("pasuwado/styles.css") as styles_file:
     styles_content = styles_file.read().encode()
@@ -19,7 +25,7 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
 
-        content = index_content.replace("{password}", generator.generate(16)).encode()
+        content = index_template({"password": generator.generate(16)}).encode()
 
         match self.path:
             case "/styles.css":
